fce32d2b36deb5e6e01ea670813bed2690f99a39
Stephane Van Pe <stephanevanpe@gmail.com>
2020-04-15 16:30:35 +0200

    the footer is done with the link to my profil

--- src/pages/Prices.jsx
+++ src/pages/Prices.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
 const Prices = () => {
 	return (
 		<div>
 			<Navbar />
 			<h1>Tarifs</h1>
+			<Footer />
 		</div>
 	);
 };
