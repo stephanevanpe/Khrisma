2d2599946f15a686a16efa32b3b64371f5b927bb
Stephane Van Pe <stephanevanpe@gmail.com>
2020-04-16 16:02:01 +0200

    creating the page home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,58 @@
 import React from 'react';
+import { Parallax } from 'react-materialize';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import Lotus from '../pictures/62a47d8e4ab364cf476f25a473b23c75.jpg';
+import Salle from '../pictures/salle.webp';
+import Lotusbloom from '../pictures/lotosblume-6e34f198-4e69-4fbb-99a0-6e5ede854fbf.jpg';
 
 const Home = () => {
 	return (
 		<div>
-      <Navbar/>
-      <Footer/>
+			<Navbar />
+			<div>
+				<Parallax
+					image={<img alt='' src={Lotusbloom} />}
+					options={{
+						responsiveThreshold: 0,
+					}}
+				/>
+				<div className='section purple lighten-5'>
+					<div className='row container'>
+						<hr />
+						<h1 className='yellow-text text-darken-3'>Khrisma Médium</h1>
+						<h2 className='header'>Tarnos</h2>
+						<p className='grey-text text-darken-3 lighten-3' style={{ fontFamily: 'Snell Roundhand, cursive', fontSize: '40px', textIndent: '2' }}>
+							<b>M</b>edium depuis toujours, je me suis rendue compte que mes aptitudes me servaient pour aider à la gestion du deuil et des blocages divers,
+							tout en tenant compte de la généalogie. Les cartes, les mots, l'écriture viennent à mon aide .
+						</p>
+						<hr />
+					</div>
+				</div>
+				<Parallax
+					image={<img alt='' src={Lotus} />}
+					options={{
+						responsiveThreshold: 0,
+					}}
+				/>
+				<div className='section purple lighten-5'>
+					<div className='row container'>
+						<hr />
+						<p className='grey-text text-darken-3 lighten-3' style={{ fontFamily: 'Snell Roundhand, cursive', fontSize: '40px', textIndent: '2' }}>
+							<b>M</b>edium depuis toujours, je me suis rendue compte que mes aptitudes me servaient pour aider à la gestion du deuil et des blocages divers,
+							tout en tenant compte de la généalogie. Les cartes, les mots, l'écriture viennent à mon aide .
+						</p>
+						<hr />
+					</div>
+				</div>
+				<Parallax
+					image={<img alt='' src={Salle} />}
+					options={{
+						responsiveThreshold: 0,
+					}}
+				/>
+			</div>
+			<Footer />
 		</div>
 	);
 };
