a98d101883ee7b20b13dd527553a88bea9a22a8c
Stephane Van Pe <stephanevanpe@gmail.com>
2020-04-17 15:25:33 +0200

    adding a title in the second card of the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,9 +23,7 @@ const Home = () => {
 					<div className='row container'>
 						<hr />
 						<h1 className='yellow-text text-darken-3'>
-							<img alt='' src={Deco1} />
-							{' '}Khrisma{' '} 
-							<img alt='' src={Deco2} />
+							<img alt='' src={Deco1} /> Khrisma <img alt='' src={Deco2} />
 						</h1>
 						<h4 className='header purple-text text-darken-4'> Medium de naissance - magnétisme holistique - coupeur de feu et de rayons - Maître Reiki</h4>
 						<p className='grey-text text-darken-3 lighten-3' style={{ fontFamily: 'Snell Roundhand, cursive', fontSize: '40px' }}>
@@ -44,6 +42,7 @@ const Home = () => {
 				<div className='section purple lighten-5'>
 					<div className='row container'>
 						<hr />
+						<h4 className='header purple-text text-darken-4'>Déroulement de la consultation</h4>
 						<p className='grey-text text-darken-3 lighten-3' style={{ fontFamily: 'Snell Roundhand, cursive', fontSize: '40px', textIndent: '2' }}>
 							<b>M</b>edium depuis toujours, je me suis rendue compte que mes aptitudes me servaient pour aider à la gestion du deuil et des blocages divers,
 							tout en tenant compte de la généalogie. Les cartes, les mots, l'écriture viennent à mon aide .
