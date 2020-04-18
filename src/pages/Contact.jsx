4c843cf4ce9bb8eddcd5366aba4abaace4b53d8d
Stephane Van Pe <stephanevanpe@gmail.com>
2020-04-18 18:17:36 +0200

    removing the form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -53,28 +53,6 @@ class Contact extends Component {
 										</Button>
 									</div>
 								</div>
-								<NetlifyForm name='Contact Form'>
-  {({ loading, error, success }) => (
-    <div>
-      {loading &&
-        <div>Loading...</div>
-      }
-      {error &&
-        <div>Your information was not sent. Please try again later.</div>
-      }
-      {success &&
-        <div>Thank you for contacting us!</div>
-      }
-      {!loading && !success &&
-        <div>
-          <input type='text' name='Name' required />
-          <textarea name='Message' required />
-          <button>Submit</button>
-        </div>
-      }
-    </div>
-  )}
-</NetlifyForm>
 								
 							</Card>
 						</Col>
