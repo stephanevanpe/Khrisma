2c52099ceeaefa0948434ce285caebde18a351f3
Stephane Van Pe <stephanevanpe@gmail.com>
2020-04-18 12:07:08 +0200

    adding capital letter in the first card of the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
 								<img alt='' src={Deco1} /> Khrisma <img alt='' src={Deco2} />
 							</h1>
 						</Col>
-						<h4 className='header purple-text text-darken-4'> Medium de naissance - magnétisme holistique - coupeur de feu et de rayons - Maître Reiki</h4>
+						<h4 className='header purple-text text-darken-4'> Medium de naissance - Magnétisme holistique - Coupeur de feu et de rayons - Maître Reiki</h4>
 						<p className='grey-text text-darken-3 lighten-3' style={{ fontFamily: 'Snell Roundhand, cursive', fontSize: '40px' }}>
 							" <b>J</b>e me suis rendue compte que mes aptitudes me servaient pour aider à la gestion du deuil et des blocages divers, tout en tenant compte
 							de la généalogie. Les cartes, les mots, l'écriture viennent à mon aide . "
