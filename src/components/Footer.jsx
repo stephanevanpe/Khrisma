a8852fcfe442ce382fc50c695881498beb54bff2
Stephane Van Pe <stephanevanpe@gmail.com>
2020-04-20 19:03:30 +0200

    adding the target blank for all the external links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Fouter = () => {
 			copyrights='©2020 Copyright tous droits réservés'
 			moreLinks={
 				<>
-					<a href='https://www.facebook.com/Khrisma-medium-316028152260673/'>
+					<a href='https://www.facebook.com/Khrisma-medium-316028152260673/' target='_blank'>
 						<Button node='button' floating waves='light' className='purple darken-4 right'>
 							<Icon left>
 								<span className='purple-text text-lighten-5' style={{ fontFamily: 'klavika', fontSize: '35px' }}>
@@ -20,11 +20,10 @@ const Fouter = () => {
 					</a>
 					<p className='purple-text text-darken-4' href='#!'>
 						{' '}
-						Krisma N° SIREN: 831 976 543
-						{' '}
-					<a className='purple-text text-darken-4' href='https://www.linkedin.com/in/stephane-van-pe/' >
-						- Réalisé par Stéphane Van Pé
-					</a>
+						Krisma N° SIREN: 831 976 543{' '}
+						<a className='purple-text text-darken-4' href='https://www.linkedin.com/in/stephane-van-pe/' target='_blank'>
+							- Réalisé par Stéphane Van Pé
+						</a>
 					</p>
 				</>
 			}
