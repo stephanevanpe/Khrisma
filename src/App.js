54fd2b33a2202b6e2a3de0f6958bdb01f4572683
Stephane Van Pe <stephanevanpe@gmail.com>
2020-04-27 17:12:40 +0200

    adding google analytics

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
+import ReactGA from 'react-ga';
 
 import Router from './router/Router';
 
+function initializeReactGA() {
+	ReactGA.initialize('G-S9DDPEFWEN');
+	ReactGA.pageview('/');
+}
 function App() {
   return (
     <div className="App">
