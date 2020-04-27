ffcf419f6fcdaa839984754771ab05fead8b7ac6
Stephane Van Pe <stephanevanpe@gmail.com>
2020-04-27 18:51:07 +0200

    google ok

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.css';
-import ReactGA from 'react-ga';
+
 
 import Router from './router/Router';
 
-function initializeReactGA() {
-	ReactGA.initialize('G-S9DDPEFWEN');
-	ReactGA.pageview('/');
-}
+
 function App() {
   return (
     <div className="App">
