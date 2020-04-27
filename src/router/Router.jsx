dc7135ba01fd36c8b0a1bef62c9813f9bd213114
Stephane Van Pe <stephanevanpe@gmail.com>
2020-04-27 16:47:21 +0200

    creating the robots.txt

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -6,7 +6,7 @@ import Prices from '../pages/Prices';
 import Contact from '../pages/Contact';
 import History from '../pages/MyHistory';
 import Error from '../pages/404';
-import Sitemap from '../pages/sitemap.xml';
+
 
 class Router extends Component {
 	render() {
@@ -17,7 +17,6 @@ class Router extends Component {
 					<Route exact path='/medium/mon-histoire' component={History} />
 					<Route exact path='/medium/tarifs' component={Prices} />
 					<Route exact path='/medium/contact' component={Contact} />
-					<Route exact path='/sitemap' component={Sitemap}/>
 					<Route exact path='/404' component={Error} />
 					<Redirect to='/404' />
 				</Switch>
