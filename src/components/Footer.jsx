8878418364217d5c323154dbca89b453415ff0c5
Stephane Van Pe <stephanevanpe@gmail.com>
2020-05-25 11:36:55 +0200

    changing the footer for the legal mention open in an over page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,7 +21,7 @@ const Fouter = () => {
 					<p className='purple-text text-darken-4' href='#!'>
 						{' '}
 						Khrisma N° SIREN: 831 976 543{' '}
-						<a className='purple-text text-darken-4' href='/LegalMention.html'>
+						<a className='purple-text text-darken-4' href='/LegalMention.html' target='_blank' rel='noopener noreferrer'>
 							- Mentions légales{' '}
 						</a>
 						<a className='purple-text text-darken-4' href='https://www.linkedin.com/in/stephane-van-pe/' target='_blank' rel='noopener noreferrer'>
