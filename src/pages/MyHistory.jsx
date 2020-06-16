f780afc5f07adf0f77d169d64ff9e423346d984b
Stephane Van Pe <stephanevanpe@gmail.com>
2020-06-16 19:33:10 +0200

    changing photo

--- src/pages/MyHistory.jsx
+++ src/pages/MyHistory.jsx
@@ -32,21 +32,21 @@ const MyHistory = () => {
 							contact quasi permanent avec lui . J'ai su à quel moment il a fait le choix de nous quitter . Je n'en ai jamais parlé à personne .
 						</p>
 						<p className='grey-text text-darken-3 lighten-3' style={{ fontFamily: 'Snell Roundhand, Bookman Old Style', fontSize: '40px' }}>
-							<b>P</b>lus tard dans ma vie , lors d'une intervention chirurgicale , j'ai eu un problème durant lequel j'ai pu entrevoir l'autre côté du
-							" Voile  " : nos êtres chers disparus , sont bien présents dans un amour totalement inconditionnel que je ne peux vous décrire que par les mots
+							<b>P</b>lus tard dans ma vie , lors d'une intervention chirurgicale , j'ai eu un problème durant lequel j'ai pu entrevoir l'autre côté du "
+							Voile " : nos êtres chers disparus , sont bien présents dans un amour totalement inconditionnel que je ne peux vous décrire que par les mots
 							amour , lumière , beauté , douceur , plénitude . Heureusement , J'ai eu la chance de rencontrer un vieux magnétiseur qui m'a pris sous son aile
 							et m'a fait comprendre bien des choses …
 						</p>
 						<img
 							class='responsive-img'
 							alt='medium tarnos khrisma'
-							src={Ange}
-							onClick={() => swal({ icon: `${Ange}`, text: 'Cette peinture a été réalisée par mes soins . D après le modéle des cartes de Doreen Virtue .' })}
+							src={Lotus}
+							onClick={() => swal({ icon: `${Lotus}`, text: 'Cette peinture a été réalisée par mes soins .' })}
 						/>
 						<p className='grey-text text-darken-3 lighten-3' style={{ fontFamily: 'Snell Roundhand, Bookman Old Style', fontSize: '40px' }}>
 							<b>E</b>nsuite , pendant deux ans , il y a eu de nombreux " départs " dans mon entourage . A cette époque j’ai rencontré une amie , qui , elle
-							aussi , avait perdu un être cher . Bercée depuis son enfance dans le domaine spirituel , j'ai pu l'aider et elle a su en retour , me
-							faire accepter ce que mes guides spirituels me demandaient . Tout simplement , d'aider mon prochain .
+							aussi , avait perdu un être cher . Bercée depuis son enfance dans le domaine spirituel , j'ai pu l'aider et elle a su en retour , me faire
+							accepter ce que mes guides spirituels me demandaient . Tout simplement , d'aider mon prochain .
 						</p>
 						<p className='grey-text text-darken-3 lighten-3' style={{ fontFamily: 'Snell Roundhand, Bookman Old Style', fontSize: '40px' }}>
 							<b>P</b>endant plusieurs années , j'ai accompagné les personnes que je rencontrais , dans ma vie privée comme professionnelle et j ai constaté
